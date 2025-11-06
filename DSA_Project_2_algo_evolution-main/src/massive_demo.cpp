fda9f73cbf06ae254a3aac3e64444af554bdf46c
Sivan Reddy Pushpagiri <sivanrp515@gmail.com>
2025-11-06 06:00:00 -0500

    Add quicksort partitioning: Lomuto and Hoare schemes

--- DSA_Project_2_algo_evolution-main/src/massive_demo.cpp
+++ DSA_Project_2_algo_evolution-main/src/massive_demo.cpp
@@ -32,3 +32,31 @@ void quicksort_simple(std::vector<int>& arr, int left, int right, Metrics& m,
   if(pivot_choice == 0) pivot_val = arr[left];
   else if(pivot_choice == 1) pivot_val = arr[right];
   else pivot_val = arr[(left + right) / 2];
+  if(partition_type == 0){
+    int pivot_idx = left;
+    for(int i = left; i < right; i++){
+      if(++m.comparisons, arr[i] < pivot_val){
+        std::swap(arr[pivot_idx], arr[i]);
+        m.swaps++;
+        pivot_idx++;
+      }
+    }
+    std::swap(arr[pivot_idx], arr[right]);
+    m.swaps++;
+    quicksort_simple(arr, left, pivot_idx - 1, m, pivot_choice, partition_type, cutoff);
+    quicksort_simple(arr, pivot_idx + 1, right, m, pivot_choice, partition_type, cutoff);
+  }else{
+    int i = left, j = right;
+    while(i <= j){
+      while(++m.comparisons, arr[i] < pivot_val) i++;
+      while(++m.comparisons, arr[j] > pivot_val) j--;
+      if(i <= j){
+      std::swap(arr[i], arr[j]);
+      m.swaps++;
+      i++; j--;
+    }
+    }
+    quicksort_simple(arr, left, j, m, pivot_choice, partition_type, cutoff);
+    quicksort_simple(arr, i, right, m, pivot_choice, partition_type, cutoff);
+  }
+}
